491e5f3f988727cfacaebf790d0fd87055c9a491
Tiago Marques <tiagomarques2283@hotmail.com>
2024-10-31 12:29:53 -0300

    Adicionando o README e adicionando um try catch

---
+++ README.md
@@ -0,0 +1,11 @@
+## Lista de Compras/Tarefas
+____
+
+#### Criação de uma página responsiva de lista de Items feita dinamicamente com JavaScritp!
+
+###### o Usuário digita os items, que podem ser adicionados e excluidos a qualquer momento! Alem disso, quando marcado o item ficará cortado por uma linha mostrando que a atividade foi concluida alem de ter um alerta na pagina informando isso a ele!
+___
+
+##### Segue a página modelo do projeto!
+
+<img src="capaProjeto.png" alt="">
--- js/main.js
+++ js/main.js
@@ -7,32 +7,38 @@ let showSucessTrashItem = document.querySelector('footer');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  let newContainerItem = document.createElement('div');
-  newContainerItem.classList.add('item_container');
+  try {
+    let newContainerItem = document.createElement('div');
+    newContainerItem.classList.add('item_container');
+  
+    let checkboxItem = document.createElement('input');
+    checkboxItem.type = 'checkbox';
+  
+    let labelItem = document.createElement('label');
+    labelItem.classList.add('nameItemCheck');
+    labelItem.textContent = inputItem.value;
+  
+    let deleteIcon = document.createElement('img');
+    deleteIcon.src = '../assets/icons/trash.svg';
+    deleteIcon.alt = 'ícone de excluir';
+  
+    deleteIcon.addEventListener('click', () => {
+      listItens.removeChild(newContainerItem);
+      showSucessTrashItem.classList.add('showSucessTrash');
+  
+      setTimeout(() => {
+        showSucessTrashItem.classList.remove('showSucessTrash');
+      }, 1500);
+  
+    });
+  
+    newContainerItem.append(checkboxItem, labelItem, deleteIcon);
+    listItens.append(newContainerItem);
+  
+    inputItem.value = '';
+    
+  } catch (error) {
+    alert("Não foi possivel adicionar o Item =(. Por favor tente mais tarde!");
+  }
 
-  let checkboxItem = document.createElement('input');
-  checkboxItem.type = 'checkbox';
-
-  let labelItem = document.createElement('label');
-  labelItem.classList.add('nameItemCheck');
-  labelItem.textContent = inputItem.value;
-
-  let deleteIcon = document.createElement('img');
-  deleteIcon.src = '../assets/icons/trash.svg';
-  deleteIcon.alt = 'ícone de excluir';
-
-  deleteIcon.addEventListener('click', () => {
-    listItens.removeChild(newContainerItem);
-    showSucessTrashItem.classList.add('showSucessTrash');
-
-    setTimeout(() => {
-      showSucessTrashItem.classList.remove('showSucessTrash');
-    }, 1000);
-
-  });
-
-  newContainerItem.append(checkboxItem, labelItem, deleteIcon);
-  listItens.append(newContainerItem);
-
-  inputItem.value = '';
 });
